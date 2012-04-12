d01768352297f2257b92c96d94d49d4c36a02d7f
redfire1239 <redfire1239@gmail.com>
2012-04-12 17:29:36 +0000

    abcdefgh :))

--- Source/Alpha/MyGame3D_0912100/MyGame3D_0912100/MyGame.cs
+++ Source/Alpha/MyGame3D_0912100/MyGame3D_0912100/MyGame.cs
@@ -168,7 +168,7 @@ namespace MyGame3D_0912100
                                      new Vector3(0, 50, 0),
                                      new Vector3(0, 0, 0),
                                      new Vector3(0, -35, 0),
-                                     new Vector3(0, 0, 0),
+                                     new Vector3(55, 0, 0),
                                  };
 
             string texturePrefix1 = "OptionMenu\\";
@@ -176,7 +176,7 @@ namespace MyGame3D_0912100
                                   new Vector2(320/2, 52/2),
                                   new Vector2(320/2, 52/2),
                                   new Vector2(320/2, 52/2),
-                                  new Vector2(20, 20)
+                                  new Vector2(15, 15)
                               };
             this.optionMenus = new OptionMenu(Content, texturePrefix1, textures1, positions1, sizes1);
             this._camera = new PerspectiveCamera(
